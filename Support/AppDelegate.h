60e66900c00c63824acea45e301c7d0718a0c689
Vladislav Kleschenko <Uladzislau_Kleshchanka@epam.com>
2018-04-29 12:10:18 +0300

    init

---
+++ Support/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  initials-project-ios
+//
+//  Created by Uladzislau Kleshchanka on 4/29/18.
+//  Copyright © 2018 Uladzislau Kleshchanka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Support/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  initials-project-ios
+//
+//  Created by Uladzislau Kleshchanka on 4/29/18.
+//  Copyright © 2018 Uladzislau Kleshchanka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
